a17aaab906b32ae9465dbd7af83a505d909a5ae1
Viczán Balázs <hamstermajszter@gmail.com>
2018-12-03 09:02:02 +0100

    Envelope model introduced

--- src/app/envelope/envelope-list/envelope-list.component.ts
+++ src/app/envelope/envelope-list/envelope-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Envelope } from '../envelope.model';
+
 @Component({
   selector: 'app-envelope-list',
   templateUrl: './envelope-list.component.html',
   styleUrls: ['./envelope-list.component.scss']
 })
 export class EnvelopeListComponent implements OnInit {
+
+  envelopes: Envelope[] = [
+    new Envelope('Household', 80000),
+    new Envelope('Fun', 50000)
+  ];
 
   constructor() { }
 
---
+++ src/app/envelope/envelope.model.ts
@@ -0,0 +1,11 @@
+export class Envelope {
+    public name: string;
+    public balance: number;
+
+
+	constructor(name: string, balance: number) {
+        this.name = name;
+        this.balance = balance
+    }
+    
+}
